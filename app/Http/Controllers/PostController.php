f630eff0f875551186615efd3d67332bc3512227
yoshiki1125 <jituhiro1125@gmail.com>
2023-01-13 16:37:32 +0900

    面談

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,33 +10,38 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
+   　　 return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
     
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
+    
     public function create()
     {
         return view('posts/create');
     }
+    
     public function store(PostRequest $request, Post $post)
     {
-       $input = $request['post'];
+        $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
     public function edit(Post $post)
     {
         return view('posts/edit')->with(['post' => $post]);
     }
+    
     public function update(PostRequest $request, Post $post)
     {
         $input_post = $request['post'];
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
+    
     public function delete(Post $post)
     {
         $post->delete();
